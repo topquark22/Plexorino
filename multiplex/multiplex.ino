8ff56200bbb16c674dc207ef1f87f748a1a4ca71
topquark22 <topquark22@yahoo.ca>
2024-02-27 06:18:18 -0500

    add compiler directive to select number of 74HC259 chip banks

--- multiplex/multiplex.ino
+++ multiplex/multiplex.ino
@@ -2,13 +2,24 @@
  * Interface to LM259 multiplex latch chip
  */
 
+// remove this if you use pin A2 for something else
+#define USE_BANK2
+
+#ifdef USE_BANK2
+  #define NUM_LINES 16
+#else
+  #define NUM_LINES 8
+#endif
+
 // 74HC259 pins: (note we don't use any PWM-capable output pins, SPI or I2C pins)
 const uint8_t PIN_XA0 = 2;       // 1's bit of line address (pin 1 of 74HG259)
 const uint8_t PIN_XA1 = 4;       // 2's bit of line address (pin 2 of 74HC259)
 const uint8_t PIN_XA2 = 7;       // 4's bit of line address (pin 3 of 74HC259)
 const uint8_t PIN_XDATA = 8;     // output data (pin 13 of 74HC259)
 const uint8_t PIN_XLATCH_0 = A3; // latch enable (pin 14 of 74HC259 #1 for outputs 0-7)
-const uint8_t PIN_XLATCH_8 = A2; // latch enable (pin 14 of 74HC259 #2 for outputs 8-15)
+#ifdef USE_BANK2
+const uint8_t PIN_XLATCH_8 = A2; // latch enable (pin 14 of 74HC259 #2 for outputs 8-15,)
+#endif
 
 const unsigned int LATCH_DLY = 5; // Latch hold-down time (microseconds)
 
@@ -18,6 +29,7 @@ void write259(uint16_t addr, bool value) {
   digitalWrite(PIN_XA2, (addr & 0x4) >> 2);
   digitalWrite(PIN_XDATA, value);
   // latch data onto output line
+#ifdef USE_BANK2
   if (addr & 0x8) {
     // latch data on chip #1 for lines 8-15
     digitalWrite(PIN_XLATCH_8, LOW);
@@ -29,11 +41,17 @@ void write259(uint16_t addr, bool value) {
     delayMicroseconds(LATCH_DLY);
     digitalWrite(PIN_XLATCH_0, HIGH);
   }
+#else
+  // latch data on chip #2 for lines 0-7
+  digitalWrite(PIN_XLATCH_0, LOW);
+  delayMicroseconds(LATCH_DLY);
+  digitalWrite(PIN_XLATCH_0, HIGH);
+#endif
 }
 
 // write a 16-bit value across all 16 output lines
 void writeBits(uint16_t value) {
-  for (int addr = 0; addr < 16; addr++) {
+  for (int addr = 0; addr < NUM_LINES; addr++) {
     write259(addr, (value >> addr) & 0x1);
   }
 }
@@ -50,14 +68,16 @@ void setup() {
   pinMode(PIN_XA2, OUTPUT);
   pinMode(PIN_XDATA, OUTPUT);
   pinMode(PIN_XLATCH_0, OUTPUT);
+#ifdef USE_BANK2
   pinMode(PIN_XLATCH_8, OUTPUT);
+#endif
   reset();
 }
 
 void loop() {
   // blink all 16 lines in sequence
   const int ms = 500;
-  for (int addr = 0; addr < 16; addr++) {
+  for (int addr = 0; addr < NUM_LINES; addr++) {
     write259(addr, HIGH);
     delay(ms);
     write259(addr, LOW);
