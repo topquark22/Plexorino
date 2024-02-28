21fb14a4a9213a34ff5cbb7420f0531ec9493018
topquark22 <topquark22@yahoo.ca>
2024-02-28 16:36:35 -0500

    refactor/rename

--- mux16/mux16.ino
+++ mux16/mux16.ino
@@ -2,27 +2,29 @@
  * Interface to 74LS150 16-to-1 multiplexer chip
  */
 
-const uint8_t PIN_MA0 = 2; // addr 1's bit, to mux pin 15
-const uint8_t PIN_MA1 = 3; // addr 2's bit, to mux pin 14
-const uint8_t PIN_MA2 = 4; // addr 4's bit, to mux pin 13
-const uint8_t PIN_MA3 = A1; // addr 8's bit, to mux pin 11
-const uint8_t PIN_MD_ = A0; // data read, to mux pin 10 (inverted)
+// address bus
+const uint8_t PIN_ADDR0 = 2; // mux pin 15
+const uint8_t PIN_ADDR1 = 3; // mux pin 14
+const uint8_t PIN_ADDR2 = 4; // mux pin 13
+const uint8_t PIN_ADDR3 = A1; //mux pin 11
+// data bus
+const uint8_t PIN_DATA = A0; // mux pin 10
 
-// read the input at mux address addr
+// read the input at mux addr. The value is inverted
 bool readMux16(uint8_t addr) {
-  digitalWrite(PIN_MA0, addr & 0x1);
-  digitalWrite(PIN_MA1, (addr >> 1) & 0x1);
-  digitalWrite(PIN_MA2, (addr >> 2) & 0x1);
-  digitalWrite(PIN_MA3, (addr >> 3) & 0x1);
-  return !digitalRead(PIN_MD_);
+  digitalWrite(PIN_ADDR0, addr & 0x1);
+  digitalWrite(PIN_ADDR1, (addr >> 1) & 0x1);
+  digitalWrite(PIN_ADDR2, (addr >> 2) & 0x1);
+  digitalWrite(PIN_ADDR3, (addr >> 3) & 0x1);
+  return !digitalRead(PIN_DATA);
 }
 
  void setup() {
-  pinMode(PIN_MA0, OUTPUT);
-  pinMode(PIN_MA1, OUTPUT);
-  pinMode(PIN_MA2, OUTPUT);
+  pinMode(PIN_ADDR0, OUTPUT);
+  pinMode(PIN_ADDR1, OUTPUT);
+  pinMode(PIN_ADDR2, OUTPUT);
   // 74LS150 already has input pullups (refer to data sheet)
-  pinMode(PIN_MD_, INPUT_PULLUP);
+  pinMode(PIN_DATA, INPUT);
 
   // put your setup code here, to run once
   
--- mux8/mux8.ino
+++ mux8/mux8.ino
@@ -2,25 +2,25 @@
  * Interface to 74LS151 8-to-1 multiplexer chip
  */
 
-const uint8_t PIN_MA0 = 2; // addr 1's bit, to mux pin 11
-const uint8_t PIN_MA1 = 3; // addr 2's bit, to mux pin 10
-const uint8_t PIN_MA2 = 4; // addr 4's bit, to mux pin 9
-const uint8_t PIN_MD = A0; // data read, to mux pin 5 (noninverted)
+const uint8_t PIN_ADDR0 = 2; // addr 1's bit, to mux pin 11
+const uint8_t PIN_ADDR1 = 3; // addr 2's bit, to mux pin 10
+const uint8_t PIN_ADDR2 = 4; // addr 4's bit, to mux pin 9
+const uint8_t PIN_DATA = A0; // data read, to mux pin 5 (noninverted)
 
 // read the input at mux address addr
 bool readMux8(uint8_t addr) {
-  digitalWrite(PIN_MA0, addr & 0x1);
-  digitalWrite(PIN_MA1, (addr >> 1) & 0x1);
-  digitalWrite(PIN_MA2, (addr >> 2) & 0x1);
-  return digitalRead(PIN_MD);
+  digitalWrite(PIN_ADDR0, addr & 0x1);
+  digitalWrite(PIN_ADDR1, (addr >> 1) & 0x1);
+  digitalWrite(PIN_ADDR2, (addr >> 2) & 0x1);
+  return digitalRead(PIN_DATA);
 }
 
  void setup() {
-  pinMode(PIN_MA0, OUTPUT);
-  pinMode(PIN_MA1, OUTPUT);
-  pinMode(PIN_MA2, OUTPUT);
+  pinMode(PIN_ADDR0, OUTPUT);
+  pinMode(PIN_ADDR1, OUTPUT);
+  pinMode(PIN_ADDR2, OUTPUT);
   // 74LS151 already has input pullups (refer to datasheet)
-  pinMode(PIN_MD, INPUT_PULLUP);
+  pinMode(PIN_DATA, INPUT_PULLUP);
 
   // put your setup code here, to run once
   
