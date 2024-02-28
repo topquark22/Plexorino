5fe14636ad588ed37c41c685214b6a88cebb77ab
topquark22 <topquark22@yahoo.ca>
2024-02-28 16:03:13 -0500

    rename

--- demux/demux.ino
+++ demux/demux.ino
@@ -12,35 +12,35 @@
 #endif
 
 // 74HC259 pins: (note we don't use any SPI or I2C pins)
-const uint8_t PIN_XA0 = 2;       // 1's bit of line address (pin 1 of 74HG259)
-const uint8_t PIN_XA1 = 3;       // 2's bit of line address (pin 2 of 74HC259)
-const uint8_t PIN_XA2 = 4;       // 4's bit of line address (pin 3 of 74HC259)
-const uint8_t PIN_XDATA = 8;     // data to latch (pin 13 of 74HC259)
-const uint8_t PIN_XLATCH_0 = A3; // latch enable (pin 14 of 74HC259 #1 for outputs 0-7)
+const uint8_t PIN_ADDR0 = 2;       // 1's bit of line address (pin 1 of 74HG259)
+const uint8_t PIN_ADDR1 = 3;       // 2's bit of line address (pin 2 of 74HC259)
+const uint8_t PIN_ADDR2 = 4;       // 4's bit of line address (pin 3 of 74HC259)
+const uint8_t PIN_DATA = 8;     // data to latch (pin 13 of 74HC259)
+const uint8_t PIN_LATCH0 = A3; // latch enable (pin 14 of 74HC259 #1 for outputs 0-7)
 #ifdef USE_CHIP1
-const uint8_t PIN_XLATCH_8 = A2; // latch enable (pin 14 of 74HC259 #2 for outputs 8-15)
+const uint8_t PIN_LATCH1 = A2; // latch enable (pin 14 of 74HC259 #2 for outputs 8-15)
 #endif
 
 void writeDemux(uint16_t addr, bool value) {
-  digitalWrite(PIN_XA0, addr & 0x1);
-  digitalWrite(PIN_XA1, (addr & 0x2) >> 1);
-  digitalWrite(PIN_XA2, (addr & 0x4) >> 2);
-  digitalWrite(PIN_XDATA, value);
-  // latch data onto output line
+  digitalWrite(PIN_ADDR0, addr & 0x1);
+  digitalWrite(PIN_ADDR1, (addr & 0x2) >> 1);
+  digitalWrite(PIN_ADDR2, (addr & 0x4) >> 2);
+  digitalWrite(PIN_DATA, value);
+  // latch data onto output line. The chip timings are in ns, so we should be ok
 #ifdef USE_CHIP1
   if (addr & 0x8) {
     // latch data on Chip 1 for addresses 8-15
-    digitalWrite(PIN_XLATCH_8, LOW);
-    digitalWrite(PIN_XLATCH_8, HIGH);
+    digitalWrite(PIN_LATCH1, LOW);
+    digitalWrite(PIN_LATCH1, HIGH);
   } else {
     // latch data on Chip 0 for addresses 0-7
-    digitalWrite(PIN_XLATCH_0, LOW);
-    digitalWrite(PIN_XLATCH_0, HIGH);
+    digitalWrite(PIN_LATCH0, LOW);
+    digitalWrite(PIN_LATCH0, HIGH);
   }
 #else
   // latch data on Chip 0 for addresses 0-7
-  digitalWrite(PIN_XLATCH_0, LOW);
-  digitalWrite(PIN_XLATCH_0, HIGH);
+  digitalWrite(PIN_LATCH0, LOW);
+  digitalWrite(PIN_LATCH0, HIGH);
 #endif
 }
 
@@ -58,13 +58,13 @@ void reset() {
 }
 
 void setupDemux() {
-  pinMode(PIN_XA0, OUTPUT);
-  pinMode(PIN_XA1, OUTPUT);
-  pinMode(PIN_XA2, OUTPUT);
-  pinMode(PIN_XDATA, OUTPUT);
-  pinMode(PIN_XLATCH_0, OUTPUT);
+  pinMode(PIN_ADDR0, OUTPUT);
+  pinMode(PIN_ADDR1, OUTPUT);
+  pinMode(PIN_ADDR2, OUTPUT);
+  pinMode(PIN_DATA, OUTPUT);
+  pinMode(PIN_LATCH0, OUTPUT);
 #ifdef USE_CHIP1
-  pinMode(PIN_XLATCH_8, OUTPUT);
+  pinMode(PIN_LATCH1, OUTPUT);
 #endif
   reset();
 }
