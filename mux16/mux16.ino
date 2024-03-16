431b0ce66616069aca9e180b871042f73859406b
topquark22 <topquark22@yahoo.ca>
2024-03-15 22:39:21 -0400

    #define test code

--- mux16/mux16.ino
+++ mux16/mux16.ino
@@ -2,6 +2,9 @@
  * Interface to 74LS150 16-to-1 multiplexer chip
  */
 
+// This file contains skeleton code for your own projects. Remove this to implement your own use case
+#define TEST_CODE
+
 // address bus
 const uint8_t PIN_ADDR0 = 2; // mux pin 15
 const uint8_t PIN_ADDR1 = 3; // mux pin 14
@@ -9,6 +12,10 @@ const uint8_t PIN_ADDR2 = 4; // mux pin 13
 const uint8_t PIN_ADDR3 = A1; //mux pin 11
 // data bus
 const uint8_t PIN_DATA = A0; // mux pin 10
+
+#ifdef TEST_CODE
+const uint8_t PIN_TEST = 5;
+#endif
 
 // read the input at mux addr. The value is inverted
 bool readMux16(uint8_t addr) {
@@ -28,25 +35,26 @@ bool readMux16(uint8_t addr) {
   pinMode(PIN_DATA, INPUT);
  }
 
-const uint8_t PIN_TEST = 5; // TEST CODE
-
-int muxPin = 0; // TEST CODE
+#ifdef TEST_CODE
+int muxPin = 0;
+#endif
 
  void setup() {
   setup_mux16();
 
   // put your setup code here, to run once
 
-  // TEST CODE
+#ifdef TEST_CODE
   pinMode(PIN_TEST, OUTPUT);
   Serial.begin(9600);
   Serial.println(F("Enter pin to test (default 0):"));
+#endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly
 
-  // TEST CODE
+#ifdef TEST_CODE
   if (Serial.available()) {
     muxPin = Serial.readString().toInt();
     Serial.print(F("Testing pin "));
@@ -54,5 +62,5 @@ void loop() {
     Serial.println(F("Enter next pin to test:"));
   }
   digitalWrite(PIN_TEST, readMux16(muxPin));
-
+#endif TEST_CODE
 }
