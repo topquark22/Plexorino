ed164c5e65e2d89c6a499fb72508824470fbde4d
topquark22 <topquark22@yahoo.ca>
2024-06-12 00:48:35 -0400

    modify test case

--- plexorino/plexorino.ino
+++ plexorino/plexorino.ino
@@ -1,5 +1,5 @@
 /**
- * Interface to:
+ * Test code for interface to:
  *   74LS150 16-to-1 multiplexer chip
  *   74LS151 8-to-1 multiplexer chip
  *   74HC259 8-bit demultiplexer chip
@@ -16,23 +16,28 @@ void setup() {
 }
 
 void loop() {
-  for (muxAddr_t addr = 0; addr < MUX_CT; addr++) {
-    writeDemux(addr % DEMUX_CT, !readMux(addr));
+  // start by indicating the beginning of the sequence with 3 short blinks
+  for (int i = 0; i < 3; i++) {
+    writeDemux(0, HIGH);
+    delay(50);
+    writeDemux(0, LOW);
+    delay(50);
   }
-}
-/*
-void loop() {
-  // put your main code here, to run repeatedly
+  delay(1000);
 
-// mux test routine
-  for (muxAddr_t addr = 0; addr < MUX_CT; addr++) {
+  // read each mux input in sequence. Output a short blink for a 0, a long blink for a 1
+  for (int addr = 0; addr < MUX_CT; addr++) {
+    bool v = readMux(addr);
     Serial.print(F("E"));
     Serial.print(addr);
     Serial.print(F(":"));
-    Serial.print(readMux(addr) ? F("HIGH ") : F("LOW  "));
+    Serial.print(v ? F("HIGH ") : F("LOW  "));
+    int t = !v ? 750 : 100;
+    writeDemux(0, HIGH);
+    delay(t);
+    writeDemux(0, LOW);
+    delay(1000 - t);
   }
+  delay(750);
   Serial.println();
-  delay(1000);
-
 }
-*/
