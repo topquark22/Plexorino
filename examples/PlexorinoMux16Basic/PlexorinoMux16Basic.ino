a2c8296fd438f772cf9de00caa7677c97fa70fbd
Geoffrey T. Falk <topquark22@yahoo.ca>
2026-01-10 09:04:19 -0500

    split Plexorino into PlexorinoMux and PlexorinoDemux classes

--- examples/PlexorinoMux16Basic/PlexorinoMux16Basic.ino
+++ examples/PlexorinoMux16Basic/PlexorinoMux16Basic.ino
@@ -1,25 +1,31 @@
+// PlexorinoMux16Basic.ino
 #include <Arduino.h>
 #include <Plexorino.h>
 
+/*
+  PlexorinoMux16Basic
+  ------------------
+  Reads all 16 mux channels (74LS150) and prints them to Serial.
+
+  Notes:
+    - Uses the split-class API: PlexorinoMux
+    - Explicit begin()
+    - Uses muxAddr_t for address variables
+*/
+
+PlexorinoMux mux(PlexWidth::W16);
+
 void setup() {
+  mux.begin();
   Serial.begin(115200);
-  while (!Serial) { /* wait for serial on some boards */ }
-
-  beginMux(PlexWidth::W16);
-
-  Serial.println();
-  Serial.println("PlexorinoMux16Basic");
-  Serial.println("Mux width: 16");
+  Serial.println(F("PlexorinoMux16Basic"));
 }
 
 void loop() {
-  for (uint8_t i = 0; i < muxCount(); i++) {
-    Serial.print("CH");
-    if (i < 10) Serial.print('0');
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(readMux(i) ? "HIGH" : "LOW");
+  for (muxAddr_t i = 0; i < mux.count(); i++) {
+    Serial.print(mux.read(i) ? '1' : '0');
+    if (i != (muxAddr_t)(mux.count() - 1)) Serial.print(' ');
   }
-  Serial.println("---");
-  delay(500);
+  Serial.println();
+  delay(200);
 }
